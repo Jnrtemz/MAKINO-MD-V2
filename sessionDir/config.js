3c81bcd5bd465a556b990f5850e1dcf5fcd74f9e
Jnrtemz <152595499+Jnrtemz@users.noreply.github.com>
2024-07-06 08:02:35 +0100

    Add files via upload

---
+++ sessionDir/config.js
@@ -0,0 +1,80 @@
+toconst fs = require("fs");
+const chalk = require("chalk");
+
+//to enable function - true
+//to disable function - false
+//
+global.available = false;
+global.autoReadGc = false;
+global.autoReadAll = false;
+global.antitags = false;
+
+
+//auto functioner
+global.autoTyping = false;                //make true to enable auto typing
+global.autoRecord = false;                //make true to enable auto recording
+global.groupevent = true;                //This is the new variable for controlling group event handling.
+global.statusseen = true;                 //make true to view statuses 
+
+
+global.Owner = ["𝙏𝞢𝞛𝞘❤️🌹"];
+global.OwnerNumber = ["2349039716507"];
+global.ownertag = [""]; 
+global.OwnerName =  "𝙏𝞢𝞛𝞛𝙔 🌚🫂";
+global.BotName = "𝙏𝞢𝞛𝙕😚🥰";
+global.packname = "𝙏𝞢𝞛𝞛𝙔 🌚🫂";                             //Do not change.
+global.author = "𝙏𝞢𝞛𝞘❤️🌹";                               //Do not change.
+global.BotSourceCode = "https://github.com/anonphoenix007/MAKINO-MD-V2"; //Do not change.
+global.SupportGroupLink = "https://chat.whatsapp.com/KEg0G3UUs1G39ikdyfF5Pm "; 
+global.SupportGroupLink = "https://chat.whatsapp.com/KEg0G3UUs1G39ikdyfF5Pm"; //Do not change!
+//global.dbase = process.env.DBASE || "";//Mongodb here
+global.AuthorWhatsapp = "https://wa.me/2347080968564"; //Bot creator WhatsApp link,don't change or bot will not work.
+
+global.sessID = process.env.sessID || `{"noiseKey":{"private":{"type":"Buffer","data":"+PyeGoKcAlbNoJ4CoPoqvj36e2B+1XkKI9RRwFKqP24="},"public":{"type":"Buffer","data":"tCGuzrkUxWkIuZ4W3vStr18q8MhFwA3EUIsh+a3rvTY="}},"pairingEphemeralKeyPair":{"private":{"type":"Buffer","data":"uIJA/lG+H2KFibaaPIrEq1L/5wDgdpNcHGS7v7+0Fnk="},"public":{"type":"Buffer","data":"2rJKoQAo84MA5TwyLM8OmxzR85y/YP7KB0RerhtJplk="}},"signedIdentityKey":{"private":{"type":"Buffer","data":"sA5ebNfoHTtYmpBdQEnUvWk7MGcI8H07IPUDBhSxl24="},"public":{"type":"Buffer","data":"8+ch/GE0uLXg1lylrhSmuce4QGUvStyk32Ch0j5na1k="}},"signedPreKey":{"keyPair":{"private":{"type":"Buffer","data":"uJM3O2clsgRI4Tt6/C5MHgg/26U7ulqnnIt9xN8C2Vg="},"public":{"type":"Buffer","data":"wgs7Pc+aIdUX3qukO6W7fV5FIQuFva6LSakrRpSox1U="}},"signature":{"type":"Buffer","data":"aWE/pCcOAwV+x9MJQnNuLfmZ3Ki0Aq+qvMlEHQPtqvrjurYHt6ZmGgQRGcrY2rBMLD66Y7lQDOnT5r29kg8+jQ=="},"keyId":1},"registrationId":95,"advSecretKey":"z0RGge7peC76xmrJkMnRX4luCIt2+HUFs85raqD+HAw=","processedHistoryMessages":[],"nextPreKeyId":31,"firstUnuploadedPreKeyId":31,"accountSyncCounter":0,"accountSettings":{"unarchiveChats":false},"deviceId":"w3vp0wy2RNilp6_otAzHdQ","phoneId":"3567797a-4fbb-4246-8d18-753c274dab2e","identityId":{"type":"Buffer","data":"nreER+A29gw/OMuFfRZOkFGt20M="},"registered":true,"backupToken":{"type":"Buffer","data":"FZTpyP7sRqT2K63MZgBP7gft5sM="},"registration":{},"pairingCode":"4QW6NEFY","me":{"id":"2349039716507:23@s.whatsapp.net"},"account":{"details":"CLbR8ucHEJWzmrQGGAEgACgA","accountSignatureKey":"am6l8+bCUlXIlnnOFu0H9fry/EaoUi83DujU43WxZ3U=","accountSignature":"Apuixkao3y7IqjNKHG8zPqM/yOitW8V4QxztJ7P3GsTBwL+Vs3DW9SWTiJNyCGtFHXT0QE5TfsYBy9rBpHqyAg==","deviceSignature":"B+dY+WmSaUJ6Nf1ttb2bP/lkx3bna8/zhSJzD8gTCgDzTYnspQ8g4M8Y+tr114BabKtmSmLDPGcU+LyxgxE8jw=="},"signalIdentities":[{"identifier":{"name":"2349039716507:23@s.whatsapp.net","deviceId":0},"identifierKey":{"type":"Buffer","data":"BWpupfPmwlJVyJZ5zhbtB/X68vxGqFIvNw7o1ON1sWd1"}}],"platform":"android","lastAccountSyncTimestamp":1720097187}`
+global.prefa = ["+"]
+
+//
+global.BotLogo = fs.readFileSync("./Assets/pic1.jpg");
+global.Thumb = fs.readFileSync("./Assets/pic9.jpg");
+global.Thumb1 = fs.readFileSync("./Assets/pic5.jpg");
+global.ErrorPic = fs.readFileSync("./Assets/pic7.jpg");
+
+
+//
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+
+//
+global.mess = {
+    jobdone: 'Here you go...',
+    useradmin: 'Only group Admin can use the command 😂 ',
+    botadmin: 'Make me Admin first 😌📍.',
+    botowner: 'Only my *Owner* can use this command,Dont trespass, Baka!',
+    grouponly: 'This command is only made for *Groups*, Baka!',
+    privateonly: 'This command is only made for *Private Chat*, Baka!',
+    botonly: 'Only the *Bot itself* can use this command!',
+    waiting: 'Wait a lil bit (¬_¬)ﾉ...',
+    nolink: ' provide me *link*, Baka!',
+    error: 'An error occurd!',
+    banned: 'You cant use the commands because you Are *Banned*',
+    bangc: 'This Group is *Banned* from using Commands!',
+    nonsfw: 'Dont be a pervert,idiot! This is not a NSFW enabled group!'
+    
+}
